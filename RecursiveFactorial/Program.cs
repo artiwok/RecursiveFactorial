9700d86d072fc7e36c1cf8e068a6bb949b9fd863
artiwok <artemolga@ukr.net>
2020-11-12 02:01:04 +0200

    Add project files.

---
+++ RecursiveFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static int Factorial(int x)
+        {
+            if (x == 0)
+                return 1;
+
+            int result = x;
+
+            result *= Factorial(x - 1);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(7));
+        }
+    }
+}
